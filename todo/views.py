98c22949f5cd395dc393833b7962f2fc25af5f93
Jose Lopez <jj.lopezbuilder@gmail.com>
2020-02-29 18:09:19 -0600

    Added the ability to logout.

--- todo/views.py
+++ todo/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-from django.contrib.auth import login
+from django.contrib.auth import login, logout
 from django.db  import IntegrityError
+
+
+def home(request):
+    return render(request, 'todo/home.html')
+
+
 
 def signupuser(request):
     if request.method == 'GET':
@@ -19,6 +25,15 @@ def signupuser(request):
 
         else:
             return render(request, 'todo/signupuser.html', {'form':UserCreationForm(), 'error':'Passwords did not match.'})
+
+
+
+def logoutuser(request):
+    if request.method == 'POST':
+        logout(request)
+        return redirect('home')
+
+
 
 def currenttodos(request):
     return render(request, 'todo/currenttodos.html')
